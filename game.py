8c4986cb9b4c355d9574ffe6c25ddac8cb14e8ed
GrumpyOwl <anthony.m.taormina@gmail.com>
2012-02-14 01:27:34 -0500

    keyboard movement added

--- game.py
+++ game.py
@@ -2,13 +2,28 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400,300))
+SCREENWIDTH = 400
+SCREENHEIGHT = 300
+DISPLAYSURF = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("Raid Game")
+rect = pygame.Rect(200,160,20,20)
+pygame.key.set_repeat(1,500)
 while True:  # main game loop
+	pygame.draw.rect(DISPLAYSURF, pygame.Color("black"), rect)
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
+		elif event.type == KEYDOWN:
+			if event.key == 273 and rect.top: #up
+				rect = rect.move(0,-20)
+			elif event.key == 274 and rect.bottom != SCREENHEIGHT : #down
+				rect = rect.move(0,20)
+			elif event.key == 275 and rect.right != SCREENWIDTH: #right
+				rect = rect.move(20,0)
+			elif event.key == 276 and rect.left: #left
+				rect = rect.move(-20,0)
+	pygame.draw.rect(DISPLAYSURF, pygame.Color("red"), rect)
 	pygame.display.update()
 
 
