10738cc93d9dcee7da1673d5f1ade926222c8d80
GrumpyOwl <anthony.m.taormina@gmail.com>
2012-02-14 00:02:57 -0500

    hello world set up

---
+++ game.py
@@ -0,0 +1,14 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption("Raid Game")
+while True:  # main game loop
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
+
+
